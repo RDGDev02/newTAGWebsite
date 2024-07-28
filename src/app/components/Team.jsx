be1324783289d8deed3b47d1eb76e8f2c9e14b85
Jasper Gozum <gozumjasper@gmail.com>
2024-07-28 23:09:03 +0800

    change team

--- src/app/components/Team.jsx
+++ src/app/components/Team.jsx
@@ -26,11 +26,25 @@ const Team = () => {
       role: "Co-Founder/Marketing Head",
       twitterHandle: "https://twitter.com/0xtipxie"
     },
+    
+    {
+      imageHref: "/assets/team/heaven.png",
+      name: "0xHeavensent",
+      role: "Marketing, Partnership and Social Media",
+      twitterHandle: "https://twitter.com/0xHeavensent"
+    },
     {
       imageHref: "/assets/team/Focc.png",
       name: "Focc",
       role: "Creative Director",
       twitterHandle: "https://twitter.com/focc_x"
+    },
+    
+    {
+      imageHref: "/assets/team/banan.png",
+      name: "Banan",
+      role: "Assistant Creative Director",
+      twitterHandle: "https://twitter.com/wikhandbanan"
     },
     {
       imageHref: "/assets/team/Jas.png",
@@ -38,47 +52,7 @@ const Team = () => {
       role: "Developer",
       twitterHandle: "https://twitter.com/Jas0nly"
     },
-    {
-      imageHref: "/assets/team/jam.png",
-      name: "Jam",
-      role: "Head Writer",
-      twitterHandle: "https://twitter.com/jamrdg"
-    },
-    {
-      imageHref: "/assets/team/mm.png",
-      name: "MM",
-      role: "H.R Manager",
-      twitterHandle: "https://twitter.com/immharnelle"
-    },
-    {
-      imageHref: "/assets/team/heaven.png",
-      name: "0xHeavensent",
-      role: "Marketing and Social Media Officer",
-      twitterHandle: "https://twitter.com/0xHeavensent"
-    },
-    {
-      imageHref: "/assets/team/banan.png",
-      name: "Banan",
-      role: "Senior Graphic Artist",
-      twitterHandle: "https://twitter.com/wikhandbanan"
-    },
-    {
-      imageHref: "/assets/team/eimi.png",
-      name: "Eimethyst",
-      role: "Community Moderator",
-      twitterHandle: "https://twitter.com/itsmeseri__"
-    },
-    {
-      imageHref: "/assets/team/wez.png",
-      name: "Wez",
-      role: "TAG Host",
-    },
-    {
-      imageHref: "/assets/team/bii.png",
-      name: "Papa bii",
-      role: "TAG Host",
-      twitterHandle: "https://twitter.com/papabii_rmt"
-    },
+    
     {
       imageHref: "/assets/team/kamote.png",
       name: "Master Kamote",
@@ -112,41 +86,91 @@ const Team = () => {
         </motion.p>
       </div>
       <div className="flex flex-wrap px-[3rem]">
-      {teamData.map((member, index) => (
-        <motion.div 
-        variants={teamAnimation(0.4, index * 0.2, 0.4)}
+  {teamData.slice(0, 4).map((member, index) => (
+    <motion.div 
+      key={index}
+      variants={teamAnimation(0.4, index * 0.2, 0.4)}
+      initial="initial"
+      whileInView="animate"
+      className="w-full sm:w-6/12 lg:w-3/12 mb-6 px-6  lg:px-4"
+    >
+      <div className="relative">
+        <div className="block">
+          <Image
+            width={500}
+            height={500}
+            loading='lazy'
+            className="rounded-2xl grayscale hover:grayscale-0"
+            src={member.imageHref} alt={member.name}
+          />
+          {member.twitterHandle && (
+            <div className="absolute bottom-0 right-0 mb-2 mr-2">
+              <a 
+                href={member.twitterHandle} 
+                className="text-white text-2xl mx-2 block hover:text-blue-600" 
+                target='_blank'
+              >
+                <FontAwesomeIcon icon={faTwitter} />
+              </a>
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="text-center mt-6">
+        <h4 className="text-white mb-1">
+          {member.name}
+        </h4>
+        <div className="text-gray-400 font-light mb-2">
+          {member.role}
+        </div>
+      </div>
+    </motion.div>
+  ))}
+  
+  <div className="w-full flex flex-wrap justify-center">
+    {teamData.slice(4).map((member, index) => (
+      <motion.div 
+        key={index + 4} // Adjust the key to be unique
+        variants={teamAnimation(0.4, (index + 4) * 0.2, 0.4)}
         initial="initial"
         whileInView="animate"
-         key={index} className="w-full md:w-6/12 lg:w-3/12 mb-6 px-6 sm:px-6 lg:px-4">
-          <div className="relative">
-            <div  className="block">
-              <Image 
+        className="w-full sm:w-6/12 lg:w-3/12 mb-6 px-6  lg:px-4"
+      >
+        <div className="relative">
+          <div className="block">
+            <Image
               width={500}
               height={500}
               loading='lazy'
               className="rounded-2xl grayscale hover:grayscale-0"
-                src={member.imageHref} alt={member.name}></Image>
-                {member.twitterHandle && (
+              src={member.imageHref} alt={member.name}
+            />
+            {member.twitterHandle && (
               <div className="absolute bottom-0 right-0 mb-2 mr-2">
-              <a 
-                href={member.twitterHandle} className="text-white text-2xl mx-2 block hover:text-blue-600" target='_blank'>
+                <a 
+                  href={member.twitterHandle} 
+                  className="text-white text-2xl mx-2 block hover:text-blue-600" 
+                  target='_blank'
+                >
                   <FontAwesomeIcon icon={faTwitter} />
                 </a>
               </div>
-                )}
-            </div>
+            )}
           </div>
-          <div className="text-center mt-6">
-            <h4 className="text-white mb-1">
-              {member.name}
-            </h4>
-            <div className="text-gray-400 font-light mb-2">
-              {member.role}
-            </div>
+        </div>
+        <div className="text-center mt-6">
+          <h4 className="text-white mb-1">
+            {member.name}
+          </h4>
+          <div className="text-gray-400 font-light mb-2">
+            {member.role}
           </div>
-        </motion.div>
-        ))}
-      </div>
+        </div>
+      </motion.div>
+    ))}
+  </div>
+</div>
+
     </motion.section>
   )
 }
